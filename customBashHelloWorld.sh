b64fe5ac5a04ceb7f817611ed04d2f0bdf0ebbd1
erjosito <erjosito@hotmail.com>
2017-03-22 11:11:02 +0100

    UDRs complete, bash first test

--- customBashHelloWorld.sh
+++ customBashHelloWorld.sh
@@ -1,1 +1,17 @@
-touch /home/helloworld.txt
+
+# Enable IP forwarding
+sudo -i sysctl -w net.ipv4.ip_forward=1
+
+# Enable eth1 and get an IP address
+sudo ifconfig eth1 up
+sudo dhclient
+
+# Firewall config rules
+# =====================
+# Deny forwarded ICMP
+iptables -A FORWARD -p icmp -j DROP
+# Allow forwarded traffic
+sudo iptables -A FORWARD -i eth1 -j ACCEPT
+sudo iptables -A FORWARD -o eth1 -j ACCEPT
+# SNAT for traffic coming from Internet
+sudo iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
